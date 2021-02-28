c71c0d65b939d664942cc8feed6895dcbb9e8777
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2021-02-28 20:41:58 +0200

    Render active state for per-component filters

--- packages/frontend/src/App.svelte
+++ packages/frontend/src/App.svelte
@@ -247,6 +247,7 @@
                 {#each [null, ...components] as comp}
                     <button
                         class="bg-gray-200 hover:bg-blue-700 font-bold py-2 px-4 rounded mb-1 border-r-8"
+                        class:bg-blue-200={filterByPackage === comp}
                         class:border-red-400={healths.get(comp) === false}
                         class:border-green-400={healths.get(comp) === true}
                         on:click={() => {
