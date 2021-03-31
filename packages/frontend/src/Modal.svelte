c36b6b3801d2b6667d8f6afeb3c1277337d079ba
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2021-03-31 23:48:40 +0300

    Add "Copy JSON" button to log entry modal

--- packages/frontend/src/Modal.svelte
+++ packages/frontend/src/Modal.svelte
@@ -6,6 +6,13 @@
     const close = () => {
         openLog = null;
     };
+
+    let copied = false;
+
+    const copyJson = async () => {
+        await navigator.clipboard.writeText(JSON.stringify(openLog, null, 4));
+        copied = true;
+    };
 </script>
 
 <!-- https://tailwindui.com/components/application-ui/overlays/modals -->
@@ -81,6 +88,17 @@
                     on:click={close}
                 >
                     Close
+                </button>
+                <button
+                    type="button"
+                    class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    on:click={copyJson}
+                >
+                    {#if copied}
+                        Copied JSON to clipboard!
+                    {:else}
+                        Copy JSON
+                    {/if}
                 </button>
             </div>
         </div>
