859208de28ba6c70c95e9f03a3447a25899edb55
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2021-06-30 07:03:07 +0300

    Add API route for exporting log snapshot

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -89,6 +89,13 @@ if (process.argv.includes("--unsafe-enable-test-api")) {
     });
 }
 
+app.get("/api/export", (req, res) => {
+    const data = values.map((v) => JSON.stringify(v)).join("\n");
+    const filename = `json-log-preview_export_${new Date().toISOString()}.jsonl`;
+    res.attachment(filename);
+    res.send(data);
+});
+
 app.get(
     "*",
     express.static(path.join(__dirname, "..", "..", "frontend", "public")),
