f00d0a893a26d5cf8b433d7640689ac1b8b2e487
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2021-06-30 07:03:07 +0300

    Add UI for downloading log snapshot

--- packages/frontend/src/App.svelte
+++ packages/frontend/src/App.svelte
@@ -402,14 +402,39 @@
                     </div>
                 </fieldset>
 
-                {#if heapDiagnostics}
-                    <div class="text-gray-300 mt-auto">
-                        <p class="block text-sm font-bold mb-1">Heap</p>
-                        <p class="block text-sm font-light">
-                            {heapDiagnostics}
-                        </p>
-                    </div>
-                {/if}
+                <div class="mt-auto">
+                    <p class="block text-sm font-bold mb-3 text-gray-700">
+                        <a
+                            href="/api/export"
+                            target="_blank"
+                            class="inline-flex items-center px-2.5 py-1.5 border border-gray-300 shadow-sm text-xs font-medium rounded text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                class="-ml-0.5 mr-2 h-4 w-4"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"
+                                />
+                            </svg>
+                            Download log snapshot
+                        </a>
+                    </p>
+                    {#if heapDiagnostics}
+                        <div class="text-gray-300">
+                            <p class="block text-sm font-bold mb-1">Heap</p>
+                            <p class="block text-sm font-light">
+                                {heapDiagnostics}
+                            </p>
+                        </div>
+                    {/if}
+                </div>
             </div>
         </nav>
 
