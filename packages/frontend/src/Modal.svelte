64ae5269866859080ba845421adcbbd0a190c7b3
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2021-06-30 07:03:07 +0300

    Improve Modal header visuals

--- packages/frontend/src/Modal.svelte
+++ packages/frontend/src/Modal.svelte
@@ -78,40 +78,26 @@
             aria-modal="true"
             aria-labelledby="modal-headline"
         >
+            <div
+                class="bg-gray-50 px-4 py-3 sm:px-6 h-16 border-b-2 border-gray-700"
+            >
+                <h3
+                    class="h-full text-lg leading-6 font-medium text-gray-700 sm:ml-4 flex items-center"
+                    id="modal-headline"
+                >
+                    <span> Message details </span>
+                    <span class="ml-auto">
+                        {getRelativeTimestampLabel(
+                            now - new Date(openLog.time),
+                        )}
+                    </span>
+                </h3>
+            </div>
             <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                 <div class="sm:flex sm:items-start">
                     <div
-                        class="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
-                    >
-                        <!-- Heroicon name: outline/exclamation -->
-                        <svg
-                            class="h-6 w-6 text-red-600"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor"
-                            aria-hidden="true"
-                        >
-                            <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                            />
-                        </svg>
-                    </div>
-                    <div
                         class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left flex-grow overflow-x-hidden"
                     >
-                        <h3
-                            class="text-lg leading-6 font-medium text-gray-900 mb-6"
-                            id="modal-headline"
-                        >
-                            Message details: "{openLog.msg}" &mdash;
-                            {getRelativeTimestampLabel(
-                                now - new Date(openLog.time),
-                            )}
-                        </h3>
                         <div
                             class="mt-2 bg-gray-900 text-white rounded p-6 font-mono"
                         >
@@ -125,7 +111,7 @@
                 </div>
             </div>
             <div
-                class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse"
+                class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse h-16"
             >
                 <button
                     type="button"
