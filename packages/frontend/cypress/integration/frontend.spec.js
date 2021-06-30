aeba91471c032668dfe885b467708f5d39a3bf39
Aaro Korhonen <aarokorhonen@users.noreply.github.com>
2021-06-30 07:03:07 +0300

    Add Cypress test case for websocket close

--- packages/frontend/cypress/integration/frontend.spec.js
+++ packages/frontend/cypress/integration/frontend.spec.js
@@ -7,9 +7,14 @@ context("Frontend", () => {
 
     it("loads app frame", () => {
         cy.get("[data-test-app-title]").contains("JSON Log Preview");
+        cy.get("[data-test-ws-terminated]").should("not.exist");
     });
 
-    it("shows as terminated without websocket connection", () => {
+    it("shows as terminated when websocket is closed", () => {
+        cy.request({
+            url: "http://localhost:5000/api/test/connections",
+            method: "DELETE",
+        });
         cy.get("[data-test-ws-terminated]").should("exist");
     });
 });
