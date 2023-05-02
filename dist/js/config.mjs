6f9bc816478dae18b63e2e73464feb749054f5a6
Aaro Korhonen <aaro.korhonen@reaktor.com>
2023-05-02 22:59:31 +0300

    release: Release version 0.1.2 (experimental)

---
+++ dist/js/config.mjs
@@ -0,0 +1,15 @@
+var _a, _b;
+import arg from "arg";
+const args = arg({
+    "--open": Boolean,
+    "--unsafe-enable-test-api": Boolean,
+    "--port": Number,
+});
+const defaultPort = 3001;
+const envPort = process.env.PORT ? parseInt(process.env.PORT, 10) : undefined;
+const port = (_b = (_a = args["--port"]) !== null && _a !== void 0 ? _a : envPort) !== null && _b !== void 0 ? _b : defaultPort;
+export const config = {
+    port,
+    open: args["--open"],
+    unsafeEnableTestApi: args["--unsafe-enable-test-api"],
+};
--- dist/js/index.mjs
+++ dist/js/index.mjs
@@ -1,38 +1,35 @@
-#!/usr/bin/env node
-
-const path = require("path");
-const http = require("http");
-const express = require("express");
-const WebSocket = require("ws");
-const open = require("open");
-
+import http from "node:http";
+import path from "node:path";
+import url from "node:url";
+import express from "express";
+import { WebSocketServer, WebSocket } from "ws";
+import open from "open";
+import { config } from "./config.mjs";
+import { testApiRouter } from "./test-api.mjs";
+const __dirname = url.fileURLToPath(new URL(".", import.meta.url));
 process.stdin.pipe(process.stdout);
 process.stdin.resume();
 process.stdin.setEncoding("utf8");
-
-let values = [];
-
+export let values = [];
 process.stdin.on("data", (data) => {
     const rawLines = data.replace(/\n$/, "").split("\n");
     const lines = rawLines.map((line) => parseLine(line));
     pushNewValues(lines);
 });
-
 process.stdin.on("end", () => {
     setTimeout(() => process.exit(0), 1000);
 });
-
 let exitOnSigInt = false;
 process.on("SIGINT", () => {
     if (exitOnSigInt) {
         process.exit(0);
-    } else {
+    }
+    else {
         exitOnSigInt = true;
         console.log("Press Ctrl-C again to exit");
     }
 });
-
-const pushNewValues = (newValues) => {
+export const pushNewValues = (newValues) => {
     values.push(...newValues);
     wss.clients.forEach((client) => {
         if (client.readyState === WebSocket.OPEN) {
@@ -40,74 +37,45 @@ const pushNewValues = (newValues) => {
         }
     });
 };
-
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server, path: "/api/ws" });
-
-if (process.argv.includes("--unsafe-enable-test-api")) {
-    app.use(express.json());
-
-    app.get("/api/test/messages", (_req, res) => {
-        res.json({ values });
-    });
-
-    app.post("/api/test/messages", (req, res) => {
-        try {
-            for (const value of req.body.messages) {
-                if (typeof value !== "object" || value === null) {
-                    throw new Error("Not object");
-                } else {
-                    pushNewValues([value]);
-                }
-            }
-            res.json({ status: "ok" });
-        } catch (err) {
-            console.error(err);
-            res.status(400).json({ status: "error" });
-        }
-    });
-
-    app.post("/api/test/reset", (req, res) => {
-        for (const client of wss.clients) {
-            client.close();
-        }
-        values = [];
-        res.json({ status: "ok" });
-    });
+export const wss = new WebSocketServer({ server, path: "/api/ws" });
+if (config.unsafeEnableTestApi) {
+    app.use(testApiRouter);
 }
-
 app.get("/api/export", (req, res) => {
     const data = values.map((v) => JSON.stringify(v) + "\n").join("");
     const filename = `json-log-preview_export_${new Date().toISOString()}.jsonl`;
     res.attachment(filename);
     res.send(data);
 });
-
 app.get("*", express.static(path.join(__dirname, "..", "public")));
-
 wss.on("connection", (ws) => {
     ws.send(JSON.stringify(values));
 });
-
-const port = process.env.PORT || 3001;
-server.listen(port, () => {
-    const url = `http://localhost:${port}`;
+server.listen(config.port, () => {
+    const url = `http://localhost:${config.port}`;
     console.log(`[json-log-preview] Server listening at ${url}`);
-    if (process.argv.includes("--open")) {
+    if (config.open) {
         void open(url);
     }
 });
-
 const parseLine = (line) => {
     try {
-        const value = JSON.parse(line);
+        const regex = /{.*}/g;
+        const jsonFound = line.match(regex);
+        if (!jsonFound || !jsonFound.length) {
+            throw new Error("Not object");
+        }
+        const value = JSON.parse(jsonFound[0]);
         if (typeof value !== "object" || value === null) {
             throw new Error("Not object");
-        } else {
+        }
+        else {
             return value;
         }
-    } catch (err) {
+    }
+    catch (err) {
         const newValue = {
             time: Date.now(),
             package: "not-json",
